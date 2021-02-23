ab6ca5dc9183ae47060858530435cce615c4d4cc
Ben Hedgepeth <benhh@gmx.com>
2021-02-22 18:26:44 -0800

    Fixed JS import/export modules

--- fetch_todos/items/static/items/js/fetch_todos.js
+++ fetch_todos/items/static/items/js/fetch_todos.js
@@ -1,28 +1,5 @@
 
-function createSvgDeleteButton() {
-  let svg = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
-  svg.setAttribute("width", "45px");
-  svg.setAttribute("height", "45px");
-  svg.setAttribute("class", "delete_button");
-  let line1 = document.createElementNS("http://www.w3.org/2000/svg", 'line');
-  line1.setAttribute('x1', "10");
-  line1.setAttribute('y1', "10");
-  line1.setAttribute('x2', "40");
-  line1.setAttribute('y2', "40");
-  line1.setAttribute("stroke", "red");
-  line1.setAttribute("stroke-width", '10')
-  let line2 = document.createElementNS("http://www.w3.org/2000/svg", 'line');
-  line2.setAttribute('x1', "40");
-  line2.setAttribute('y1', "10");
-  line2.setAttribute('x2', "10");
-  line2.setAttribute('y2', "40");
-  line2.setAttribute("stroke", "red");
-  line2.setAttribute("stroke-width", '10')
-  svg.appendChild(line1);
-  svg.appendChild(line2);
-  return svg;
-}
-
+import {createSvgDeleteButton} from './utils.js';
 
 function delete_todo(event) {
   const id = event.currentTarget.id.split("_")[1];
@@ -105,6 +82,7 @@ function add_todo_element(event) {
 
 button.addEventListener("click", add_todo_element);
 input.addEventListener("keypress", function(event) {
-  if (event.key === "Enter")
-    console.log("Hey");
+  if (event.key === "Enter") {
+    add_todo_element(event);  
+  }
 })
--- fetch_todos/items/static/items/js/utils.js
+++ fetch_todos/items/static/items/js/utils.js
@@ -1,2 +1,26 @@
 
-// export {createSvgDeleteButton};
+export {createSvgDeleteButton};
+
+function createSvgDeleteButton() {
+  let svg = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+  svg.setAttribute("width", "45px");
+  svg.setAttribute("height", "45px");
+  svg.setAttribute("class", "delete_button");
+  let line1 = document.createElementNS("http://www.w3.org/2000/svg", 'line');
+  line1.setAttribute('x1', "10");
+  line1.setAttribute('y1', "10");
+  line1.setAttribute('x2', "40");
+  line1.setAttribute('y2', "40");
+  line1.setAttribute("stroke", "red");
+  line1.setAttribute("stroke-width", '10')
+  let line2 = document.createElementNS("http://www.w3.org/2000/svg", 'line');
+  line2.setAttribute('x1', "40");
+  line2.setAttribute('y1', "10");
+  line2.setAttribute('x2', "10");
+  line2.setAttribute('y2', "40");
+  line2.setAttribute("stroke", "red");
+  line2.setAttribute("stroke-width", '10')
+  svg.appendChild(line1);
+  svg.appendChild(line2);
+  return svg;
+}
